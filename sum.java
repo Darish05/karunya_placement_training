78eb275376c35a503e6895188fdc2f8bc73e6dc1
Darish Jerlin J <darishjerlin2005@gmail.com>
2025-09-09 16:05:04 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a =sc.nextInt();
+        int b= sc.nextInt();
+        float c=sc.nextFloat();
+        float d=sc.nextFloat();
+        System.out.print(a+b);
+        System.out.print(" ");
+        System.out.println(a-b);
+        System.out.print(c+d," ");
+        System.out.print(" ");
+        System.out.print(c-d);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
