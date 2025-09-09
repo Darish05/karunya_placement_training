b070565a5446a557e112796962e93a184988e4a8
Darish Jerlin J <darishjerlin2005@gmail.com>
2025-09-09 16:02:04 +0530

    Create hi.java

---
+++ hi.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        int a=sc.nextInt();
+        if(a%3==0 && a%5==0)
+            System.out.print("HiHello");
+        else if(a%3==0)
+            System.out.print("Hi");
+        else if(a%5==0)
+            System.out.print("Hello");
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
